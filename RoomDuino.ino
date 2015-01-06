80abd680a9df8122a49db22041edb4ba8b94db08
Éverton Inocêncio <hewerthomn@gmail.com>
2015-01-05 20:19:39 -0400

    implementado envio por post dos valores dos sensores

--- RoomDuino.ino
+++ RoomDuino.ino
@@ -15,178 +15,187 @@
 #include <dht11.h>
 #include <Ethernet.h>
 
-char placename[] = "Room";
+String Placename = "Room";
+//#define LOCAL 1
+
+#ifdef LOCAL
+IPAddress server(10, 1, 1, 8);
+char apiPath[] = "/roomduino/api/";
+#endif
+#ifndef LOCAL
+char server[] = "roomduino.hewertho.mn";
+char apiPath[] = "/api/";
+#endif
 
 /*
 * Ethernet Shield Configs
 */
 EthernetClient client;
-byte mac[] = { 0xDE, 0xAD, 0xBA, 0xEF, 0xFE, 0xBA };
+byte mac[] = { 0xDE, 0xAD, 0xBA, 0xEF, 0xFE, 0xBE };
 IPAddress ip(10, 1, 1, 25);
 IPAddress gateway(10, 1, 1, 1);
 IPAddress subnet(255, 255, 255, 0);
 
-//IPAddress server(10, 1, 1, 8);
-//char apiPath[] = "/roomduino/api/";
-char server[] = "roomduino.hewertho.mn";
-char apiPath[] = "/api/";
-
 int failedConnect = 0;
 int failedConnectsRetry = 5;
+boolean hasStats = false; // until this is true default text will be displayed
 
 /* Sensor Temperature */
 #define pinDHT11 9
 dht11 DHT11;
-int Temperature = 0;
-int Humidity = 0;
 
 /* Sensor Light */
 #define pinLight 0
-int lightLevel;
+int Photocell;
 
 /*
-* Variable of contents
+* Variable of sensors
 */
+int Temperature = 0;
+int Humidity = 0;
 char Date[] = "Dez, 31";
 char Time[] = "00:00";
+
 char Debug[80];
-
-boolean hasStats = false; // until this is true default text will be displayed
-
 long updateFrequency = 5 * 60000; // 60000 = 1 minute
+String data;
 
 void(* reset) (void) = 0;
 
 void setup()
 {
   Serial.begin(9600);  
-  Serial.println("Starting....");
+  Serial.println("# RoomDuino starting....");
   
-  Ethernet.begin(mac); //, ip, gateway, subnet);
+  int eth;
+  #ifdef LOCAL
+  eth = Ethernet.begin(mac, ip, gateway, subnet);
+  #endif
+  
+  #ifndef LOCAL
+  eth = Ethernet.begin(mac); //, ip, gateway, subnet);
+  if(eth == 0)
+  {
+    Serial.println("# Failed DHCP connection. Restarting...");
+    delay(3000); reset();
+  }
+  #endif
   
   delay(3000);  
-  Serial.print("IP local: "); Serial.println(Ethernet.localIP());
+  Serial.print("# IP local: "); Serial.println(Ethernet.localIP());
 }
 
 void loop()
 {
-  printVars();
+  printSensorData();
+    
   getTemperature();
-  getLight();
+  getPhotocell();
   
   connectServer();
   
   checkFailedsConnect();
+  #ifdef LOCAL
+  delay(3000);
+  #endif
+  
+  #ifndef LOCAL
   delay(updateFrequency);
+  #endif
+}
+
+void printSensorData()
+{
+  Serial.println("");Serial.println("# ------------------------------------");
+  Serial.print("# Date "); Serial.print(Date); Serial.print("  Time "); Serial.println(Time);
+  Serial.print("# Photocell: "); Serial.println(Photocell);
+  Serial.print("# Temperature "); Serial.print(Temperature); Serial.print("  /  Humidity "); Serial.print(Humidity); Serial.println("%");  
+  Serial.print("# Failed connects: "); Serial.println(failedConnect);
+  Serial.println("");
 }
 
 void getTemperature()
 { 
+  Serial.print("# DHT11 ");
   int chk = DHT11.read(pinDHT11);
-  
+
   switch(chk)
   {
     case DHTLIB_OK:
       Temperature = DHT11.temperature;
       Humidity = DHT11.humidity;
+      Serial.println("Ok!");
     break;
     
     case DHTLIB_ERROR_CHECKSUM:
-      Serial.println("# DHT11 Checksum Error");
+      Serial.println("Checksum Error");
     break;
     
     case DHTLIB_ERROR_TIMEOUT:
-      Serial.println("# DHT11 Timeout");
+      Serial.println("Timeout");
     break;
     
     default:
-      Serial.println("# DHT11 Unknown Error");
+      Serial.println("Unknown Error");
     break;
   }
 }
 
-void getLight()
+void getPhotocell()
 {
-  lightLevel = analogRead(pinLight);
-}
-
-void printVars()
-{
-  Serial.println("");Serial.println("------------------------------------");
-  Serial.print("Date "); Serial.print(Date); Serial.print("  Time "); Serial.println(Time);
-  Serial.print("Light level: "); Serial.println(lightLevel);
-  Serial.print("Temperature "); Serial.print(Temperature); Serial.print("  Humidity "); Serial.print(Humidity); Serial.println("%");  
-  Serial.print("Connects failed: "); Serial.println(failedConnect);
-  Serial.println("");
+  Photocell = analogRead(pinLight);
 }
 
 void connectServer()
 {  
-  Serial.print("Conectando....");  
+  Serial.print("# Ethernet Connect....");  
   int conn = client.connect(server, 80);
   if(conn)
   {
-    Serial.println("Ok!");
-    delay(500);
+    Serial.println("Ok!"); delay(500);
     
-    sendLight();
-    clearClient();
+    postSensorData();
+    extractData();
     
-    sendTemperature();
-    extractData();    
+    client.stop();
     
     Serial.println("Done!");
   }
   else
   {
     failedConnect++;
-    Serial.println("Erro!");
-    return;
+    Serial.println("Error!");
+    Serial.print("# Ethernet Connect Error: "); Serial.println(conn);
   }
 }
 
-void sendTemperature()
+void postSensorData()
 {
-  String params = "T=";
-  params = params + Temperature;
-  params = params + "&H=" + Humidity;
+  data = "placename=" + Placename;
+  data = data + "&temperature=" + Temperature;
+  data = data +"&humidity=" + Humidity;
+  data = data + "&photocell=" + Photocell;
   
-  Serial.print("Enviando temperaturas....");  
-  sendRequest("GET", "temperatures", params);
-  Serial.println("Ok!");
-}
-
-void sendLight()
-{
-  String params = "name=photocell&value=";
-  params = params + lightLevel;
+  Serial.print("# Post data: "); Serial.println(data);
   
-  Serial.print("Enviando light level...");
-  sendRequest("GET", "sensors", params);
-  Serial.println("Ok!");
-}
-
-void sendRequest(char method[], char path[], String params)
-{
-  client.print(method);
-  client.print(" ");
+  client.print("POST ");
   client.print(apiPath);
-  client.print(path);
-  client.print("?P=");
-  client.print(placename);
-  client.print("&");
-  client.print(params);
+  client.print("data");
   client.println(" HTTP/1.1");
   client.print("Host: ");
   client.println(server);
-  client.println("User-Agent: Arduino");
+  client.println("User-Agent: Arduino/1.0");
   client.println("Connection: close");
-  client.println("");
+  client.println("Content-type: application/x-www-form-urlencoded");
+  client.print("Content-Length: ");
+  client.println(data.length());
+  client.println();
+  client.println(data);
 }
 
 void clearClient()
 {
-  if(client.available()) client.read();  
+  while(client.available()) client.read();  
 }
 
 void extractData()
@@ -198,7 +207,7 @@ void extractData()
   int i = 0;
   char c;
   
-  Serial.print("Extraindo dados...."); 
+  Serial.print("# Extracting server data..."); 
   
   while(client.connected() && !endFlag)  
   {
@@ -231,8 +240,7 @@ void extractData()
         Serial.print(".");
       }
     }    
-  }  
-  client.stop();  
+  }
   
   Serial.println("Ok!");
 }
@@ -240,7 +248,7 @@ void extractData()
 void setStatValue(int order, char value[])
 {
   int len = 0;  
-  //Serial.print("order: ");Serial.print(order);Serial.print(" value: ");Serial.println(value);
+  //Serial.print("# order: ");Serial.print(order);Serial.print(" value: ");Serial.println(value);
   
   switch(order)
   {
@@ -260,7 +268,7 @@ void setStatValue(int order, char value[])
       for(int i=0; i<80; i++) {
         Debug[i] = value[i];
       }
-      Serial.print("#DEBUG# "); Serial.println(Debug);
+      Serial.print("# DEBUG: "); Serial.println(Debug);
     break;
   }
 }
@@ -269,7 +277,7 @@ void checkFailedsConnect()
 {
   if(failedConnect == failedConnectsRetry)
   {
-    Serial.println("# Connect failed reach limit. Will reset now... #");
-    delay(5000); reset();
+    Serial.println("# Connects failed reach limit. Restarting...");
+    delay(3000); reset();
   }
 }
